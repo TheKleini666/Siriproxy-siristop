8431af3689fae5142d639311dc2c300d05fd5c46
TheKleini666 <thiemo.klein@gmx.at>
2012-01-06 17:16:19 +0100

    Update lib/siriproxy-siristop.rb

--- lib/siriproxy-siristop.rb
+++ lib/siriproxy-siristop.rb
@@ -5,7 +5,7 @@ require 'pp'
 
 class SiriProxy::Plugin::Siristop < SiriProxy::Plugin
         
- listen_for /stop siri proxy|stopp siri proxy|/i do
+ listen_for /stop siri proxy/i do
     say "Trying to stop siri...lat: #{object["properties"]["latitude"]}, long: #{object["properties"]["longitude"]}"
 
 puts "Stoping siri proxy..."
